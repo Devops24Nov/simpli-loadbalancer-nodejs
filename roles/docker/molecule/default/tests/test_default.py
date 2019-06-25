05b16e2c96393c40de3bad3e001e7d5172f35e17
quickfix <devops.july2.2017@gmail.com>
2019-06-25 18:33:26 +0530

    Update test_default.py

--- roles/docker/molecule/default/tests/test_default.py
+++ roles/docker/molecule/default/tests/test_default.py
@@ -9,31 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 @pytest.mark.parametrize('pkg', [
   'docker',
-  'firewalld'
 ])
 def test_pkg(host, pkg):
     package = host.package(pkg)
-
     assert package.is_installed
 
 
 @pytest.mark.parametrize('svc', [
   'docker',
-  'firewalld'
 ])
 def test_svc(host, svc):
     service = host.service(svc)
-
     assert service.is_running
-    assert service.is_enabled
 
 
-@pytest.mark.parametrize('file, content', [
-  ("/etc/firewalld/zones/public.xml", "<service name=\"http\"/>"),
-  ("/var/www/html/index.html", "Managed by Ansible")
-])
-def test_files(host, file, content):
-    file = host.file(file)
-
-    assert file.exists
-    assert file.contains(content)
