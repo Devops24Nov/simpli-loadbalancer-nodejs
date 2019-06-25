f32a658e1121025626e466612ad3917bd28bfef7
quickfix <devops.july2.2017@gmail.com>
2019-06-25 18:31:24 +0530

    Update test_default.py

--- roles/app/molecule/default/tests/test_default.py
+++ roles/app/molecule/default/tests/test_default.py
@@ -7,29 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('pkg', [
-  'npm',
-])
-def test_pkg(host, pkg):
-    package = host.package(pkg)
-    assert package.is_installed
 
 
 @pytest.mark.parametrize('svc', [
-  'npm',
+  'app',
 ])
 def test_svc(host, svc):
     service = host.service(svc)
     assert service.is_running
     assert service.is_enabled
-
-
-@pytest.mark.parametrize('file, content', [
-  ("/etc/firewalld/zones/public.xml", "<service name=\"http\"/>"),
-  ("/var/www/html/index.html", "Managed by Ansible")
-])
-def test_files(host, file, content):
-    file = host.file(file)
-
-    assert file.exists
-    assert file.contains(content)
